c712eb9524be077f9f839ea3f4392199ec48c8fc
Angie Rodriguez <angrodri902@gmail.com>
2021-03-21 23:25:30 -0700

    Add Navbar and Footer components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
@@ -17,6 +21,10 @@ export default function Home() {
         repellendus perspiciatis placeat officia ipsam veritatis assumenda
         necessitatibus, non aliquid dignissimos nihil.
       </p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
+      <Footer />
     </div>
   );
 }
