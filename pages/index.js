1d6828cbe35ac8c75dd2db600ee821484ada7cec
Angie Rodriguez <angrodri902@gmail.com>
2021-03-22 23:03:07 -0700

    Add Layout component and modify styles with CSS modules

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,26 @@
 import Head from "next/head";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <h1>Homepage</h1>
-      <p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
         iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
         repellendus perspiciatis placeat officia ipsam veritatis assumenda
         necessitatibus, non aliquid dignissimos nihil.
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
         iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
         repellendus perspiciatis placeat officia ipsam veritatis assumenda
         necessitatibus, non aliquid dignissimos nihil.
       </p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
