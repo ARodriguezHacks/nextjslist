c08b43dca21e04e33ec682e38e46487a11179004
Angie Rodriguez <angrodri902@gmail.com>
2021-03-24 23:36:16 -0700

    Replace logo and add meta elements

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,28 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
-        iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
-        repellendus perspiciatis placeat officia ipsam veritatis assumenda
-        necessitatibus, non aliquid dignissimos nihil.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
-        iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
-        repellendus perspiciatis placeat officia ipsam veritatis assumenda
-        necessitatibus, non aliquid dignissimos nihil.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Next List | About</title>
+        <meta name="keywords" content="nextjs" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
+          iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
+          repellendus perspiciatis placeat officia ipsam veritatis assumenda
+          necessitatibus, non aliquid dignissimos nihil.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
+          iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
+          repellendus perspiciatis placeat officia ipsam veritatis assumenda
+          necessitatibus, non aliquid dignissimos nihil.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -4,23 +4,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
-        iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
-        repellendus perspiciatis placeat officia ipsam veritatis assumenda
-        necessitatibus, non aliquid dignissimos nihil.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
-        iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
-        repellendus perspiciatis placeat officia ipsam veritatis assumenda
-        necessitatibus, non aliquid dignissimos nihil.
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Next List | Home</title>
+        <meta name="keywords" content="nextjs" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
+          iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
+          repellendus perspiciatis placeat officia ipsam veritatis assumenda
+          necessitatibus, non aliquid dignissimos nihil.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, minima
+          iure! Tenetur, blanditiis. Neque quidem mollitia facere recusandae
+          repellendus perspiciatis placeat officia ipsam veritatis assumenda
+          necessitatibus, non aliquid dignissimos nihil.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
