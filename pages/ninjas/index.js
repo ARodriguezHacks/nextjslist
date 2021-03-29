b8645a8586a8e440989ca89b20840fde57927ffa
Angie Rodriguez <angrodri902@gmail.com>
2021-03-29 15:09:49 -0700

    Prepare for deployment

---
+++ pages/ninjas/index.js
@@ -0,0 +1,29 @@
+import styles from "../../styles/Ninjas.module.css";
+import Link from "next/link";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+// getStaticProps is a special Nextjs function. It is only run at build-time. I take it build-time means when our app is being built, aka server side
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Ninjas;
